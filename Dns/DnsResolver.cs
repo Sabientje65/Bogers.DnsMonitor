6ff6719ea16320b38e97f32eb33b3f7c335919d2
Danny Bogers <dannybogers4@gmail.com>
2024-03-31 13:59:22 +0200

    Move cache to separate file

--- Dns/DnsResolver.cs
+++ Dns/DnsResolver.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using System.Net.Sockets;
-using Microsoft.Data.Sqlite;
 
 namespace Bogers.DnsMonitor.Dns;
 
@@ -8,7 +7,11 @@ public class DnsResolver
 {
     public static async Task QueryResourceRecords(string hostName)
     {
-        await SqliteCache.Initialize();
+        await SqliteResolverCache.Initialize();
+        await SqliteResolverCache.Clean();
+        
+        // start of by normalizing our hostname
+        if (!hostName.EndsWith('.')) hostName += '.';
         
         // 1.1.1.1, cloudflare dns, 00000001_00000001_00000001_00000001
         long myRouterIp = ((long)192 << 0 | ((long)168 << 8) | ((long)1 << 16) | ((long)1 << 24));
@@ -20,7 +23,7 @@ public class DnsResolver
         // udp.Connect(ns);
 
         // var msg = Message.Request(new Question("bogers.online", RecordType.A));
-        var myIPV4 = await QueryARecord(udp, "chapoco.bogers.online", ns);
+        var myIPV4 = await QueryARecord(udp, "chapoco.bogers.online.", ns);
         Console.WriteLine($"IPV4 for bogers.online is {myIPV4}");
     }
 
@@ -30,24 +33,25 @@ public class DnsResolver
         IPEndPoint to
     )
     {
-        var answer = await SqliteCache.Find(name, RecordType.A);
+        var answer = await SqliteResolverCache.FindFirst(name, RecordType.A);
         if (answer != null) return answer.Data;
         
         var request = Message.Request(new Question(name, RecordType.A));
         await udp.SendAsync(MessageSerializer.Serialize(request), to);
         var responseBuffer = await udp.ReceiveAsync();
         var response = MessageSerializer.Deserialize(responseBuffer.Buffer);
-
-        answer = response.Answer.FirstOrDefault(x => x.Type == RecordType.A) ?? 
-                     response.Additional.FirstOrDefault(x => x.Type == RecordType.A && x.Data.Equals(name));
-
+        
+        // first, cache everything in our response for quicker future lookups
+        foreach (var rr in response.Answer) await SqliteResolverCache.Add(rr); 
+        foreach (var rr in response.Authority) await SqliteResolverCache.Add(rr); 
+        foreach (var rr in response.Additional) await SqliteResolverCache.Add(rr); 
+        
+        answer = response.Answer.FirstOrDefault(x => x.Type == RecordType.A);
         if (answer != null) return answer.Data;
-
-        foreach (var rr in response.Answer.Where(x => x.Type == RecordType.A)) await SqliteCache.Add(rr);
-        foreach (var rr in response.Additional.Where(x => x.Type == RecordType.A)) await SqliteCache.Add(rr);
 
         foreach (var ns in response.Authority)
         {
+            // assumed to always be true
             if (ns.Type != RecordType.NS) continue;
 
             // take A records for ease, can also take AAAA as fallback?
@@ -67,134 +71,6 @@ public class DnsResolver
         }
 
         return null;
-    }
-
-    /// <summary>
-    /// SQLite based cache implementation
-    /// </summary>
-    private class SqliteCache
-    {
-        private static string ConnectionString = """Data Source=.\dnscache.db; Pooling=True""";
-
-        /// <summary>
-        /// Initialize the SQLite cache, ensuring the necessary tables etc. are created
-        /// </summary>
-        public static async Task Initialize()
-        {
-            await using var con = CreateConnection();
-            await con.OpenAsync();
-            await using var cmd = con.CreateCommand();
-
-            cmd.CommandText = """
-                CREATE TABLE IF NOT EXISTS resource_records ( 
-                    name TEXT collate nocase NOT NULL
-                    , type NUMERIC NOT NULL
-                    , class NUMERIC NOT NULL
-                    , ttl NUMERIC NOT NULL 
-                    , data TEXT NOT NULL
-                    , created INTEGER DEFAULT CURRENT_TIMESTAMP
-                );
-                
-                CREATE INDEX IF NOT EXISTS resource_records_name_type_ttl_created ON resource_records (name, type, ttl, created) 
-            """;
-
-            await cmd.ExecuteNonQueryAsync();
-        }
-        
-        /// <summary>
-        /// Add a resource record to the cache
-        /// </summary>
-        /// <param name="record">Record to cache</param>
-        public static async Task Add(ResourceRecord record)
-        {
-            await using var con = CreateConnection();
-            await con.OpenAsync();
-            await using var cmd = con.CreateCommand();
-
-            cmd.CommandText = """
-                INSERT INTO resource_records (name, type, class, ttl, data)
-                values (@name, @type, @class, @tll, @data)
-            """;
-
-            cmd.Parameters.Add("@name", SqliteType.Text).Value = record.Name;
-            cmd.Parameters.Add("@type", SqliteType.Text).Value = record.Type;
-            cmd.Parameters.Add("@class", SqliteType.Text).Value = record.Class;
-            cmd.Parameters.Add("@tll", SqliteType.Text).Value = record.TimeToLive;
-            cmd.Parameters.Add("@data", SqliteType.Text).Value = record.Data;
-
-            await cmd.ExecuteNonQueryAsync();
-        }
-
-        /// <summary>
-        /// Attempt to lookup a non-expired resource record for the given domain name of the given type
-        /// </summary>
-        /// <param name="name">Domain name</param>
-        /// <param name="type">Record type <see cref="RecordType"/></param>
-        /// <returns>Resource record when found</returns>
-        public static async Task<ResourceRecord?> Find(string name, ushort type)
-        {
-            await using var con = CreateConnection();
-            await con.OpenAsync();
-            await using var cmd = con.CreateCommand();
-
-            cmd.CommandText = """
-                SELECT name, type, class, ttl, data
-                FROM resource_records
-                WHERE name = @name
-                    AND type = @type
-                    AND ttl < CURRENT_TIMESTAMP
-                LIMIT 1
-            """;
-
-            cmd.Parameters.Add("@name", SqliteType.Text).Value = name;
-            cmd.Parameters.Add("@type", SqliteType.Integer).Value = type;
-
-            await using var reader = await cmd.ExecuteReaderAsync();
-            if (!reader.Read()) return null;
-
-            return new ResourceRecord
-            {
-                Name = reader.GetString(0),
-                Type = (ushort)reader.GetInt16(1),
-                Class = (ushort)reader.GetInt16(2),
-                TimeToLive = reader.GetInt32(3),
-                Data = reader.GetString(4),
-            };
-        }
-
-        /// <summary>
-        /// Cleanup expired records from the DB
-        /// </summary>
-        public Task Vacuum()
-        {
-            return Task.CompletedTask;
-        }
-
-        private static SqliteConnection CreateConnection() => new SqliteConnection(ConnectionString);
-
-        class Config
-        {
-            public string ConnectionString { get; set; } 
-        }
-    }
-
-
-    // todo: Move cache to SQLite DB
-    private static class Cache
-    {
-        private static readonly IDictionary<string, ResourceRecord> _lookup = new Dictionary<string, ResourceRecord>();
-
-        public static Task Flush()
-        {
-            _lookup.Clear();
-            return Task.CompletedTask;
-        }
-
-        // todo: account for ttl
-        public static void Push(ResourceRecord record) => _lookup[$"{record.Name}_{record.Type}"] = record;
-        
-        public static ResourceRecord? Lookup(string name, ushort type) => _lookup.TryGetValue($"{name}_{type}", out var record) ? record : null;
-        
     }
     
     /// <summary>
---
+++ Dns/SqliteResolverCache.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Bogers.DnsMonitor.Dns;
+
+// todo: cache in memory -> flush to sqlite cache
+
+/// <summary>
+/// SQLite based cache implementation
+/// </summary>
+class SqliteResolverCache
+{
+    private static string ConnectionString = """Data Source=D:\Data\dnscache.db; Pooling=True""";
+
+    /// <summary>
+    /// Initialize the SQLite cache, ensuring the necessary tables etc. are created
+    /// </summary>
+    public static async Task Initialize()
+    {
+        await using var con = CreateConnection();
+        await con.OpenAsync();
+        await using var cmd = con.CreateCommand();
+
+        cmd.CommandText = """
+                              CREATE TABLE IF NOT EXISTS resource_records (
+                                  name TEXT collate nocase NOT NULL
+                                  , type INTEGER NOT NULL
+                                  , class INTEGER NOT NULL
+                                  , expires INTEGER NOT NULL
+                                  , ttl INTEGER NOT NULL
+                                  , data TEXT NOT NULL
+                                  , created INTEGER DEFAULT CURRENT_TIMESTAMP
+                              );
+                              
+                              CREATE INDEX IF NOT EXISTS resource_records_expires ON resource_records (expires);
+                              CREATE INDEX IF NOT EXISTS resource_records_name_type_expires_created ON resource_records (name, type, expires, created);
+                          """;
+
+        await cmd.ExecuteNonQueryAsync();
+    }
+        
+    /// <summary>
+    /// Add a resource record to the cache
+    /// </summary>
+    /// <param name="record">Record to cache</param>
+    public static async Task Add(ResourceRecord record)
+    {
+        await using var con = CreateConnection();
+        await con.OpenAsync();
+        await using var cmd = con.CreateCommand();
+
+        // only add if previous record was expired
+        cmd.CommandText = """
+                              INSERT INTO resource_records (name, type, class, ttl, expires, data)
+                              select @name, @type, @class, @tll, @expires, @data
+                              WHERE NOT EXISTS (SELECT 1 from resource_records WHERE name = @name AND type = @type AND expires < CURRENT_TIMESTAMP)
+                          """;
+
+        cmd.Parameters.Add("@name", SqliteType.Text).Value = record.Name;
+        cmd.Parameters.Add("@type", SqliteType.Integer).Value = record.Type;
+        cmd.Parameters.Add("@class", SqliteType.Integer).Value = record.Class;
+        cmd.Parameters.Add("@tll", SqliteType.Integer).Value = record.TimeToLive;
+        cmd.Parameters.Add("@expires", SqliteType.Integer).Value = DateTime.UtcNow.AddSeconds(record.TimeToLive);
+        cmd.Parameters.Add("@data", SqliteType.Text).Value = record.Data;
+
+        await cmd.ExecuteNonQueryAsync();
+    }
+
+    /// <summary>
+    /// Attempt to lookup the first non-expired resource record for the given domain name of the given type
+    /// </summary>
+    /// <param name="name">Domain name</param>
+    /// <param name="type">Record type <see cref="RecordType"/></param>
+    /// <returns>Resource record when found</returns>
+    public static async Task<ResourceRecord?> FindFirst(string name, ushort type)
+    {
+        var records = await FindAll(name, type);
+        return records.FirstOrDefault();
+    }
+
+    /// <summary>
+    /// Attempt to lookup all non-expired resource records for the given domain name of the given type
+    /// </summary>
+    /// <param name="name">Domain name</param>
+    /// <param name="type">Record type <see cref="RecordType"/></param>
+    /// <returns>All matching resource records</returns>
+    public static async Task<ResourceRecord[]> FindAll(string name, ushort type)
+    {
+        await using var con = CreateConnection();
+        await con.OpenAsync();
+        await using var cmd = con.CreateCommand();
+
+        // depending on type, one or more entries may be present for any given given name + type combination
+        cmd.CommandText = """
+                              SELECT name, type, class, ttl, data
+                              FROM resource_records
+                              WHERE name = @name
+                                  AND type = @type
+                                  AND CURRENT_TIMESTAMP < expires
+                              ORDER BY CREATED DESC
+                          """;
+
+        cmd.Parameters.Add("@name", SqliteType.Text).Value = name;
+        cmd.Parameters.Add("@type", SqliteType.Integer).Value = type;
+
+        await using var reader = await cmd.ExecuteReaderAsync();
+        var result = new List<ResourceRecord>();
+        while (await reader.ReadAsync())
+        {
+            result.Add(new ResourceRecord
+            {
+                Name = reader.GetString(0),
+                Type = (ushort)reader.GetInt16(1),
+                Class = (ushort)reader.GetInt16(2),
+                TimeToLive = reader.GetInt32(3),
+                Data = reader.GetString(4),
+            });
+        }
+        return result.ToArray();
+    }
+
+    /// <summary>
+    /// Expunge expired records from the DB
+    /// </summary>
+    public static async Task Clean()
+    {
+        await using var con = CreateConnection();
+        await con.OpenAsync();
+        await using var cmd = con.CreateCommand();
+
+        cmd.CommandText = """DELETE FROM resource_records WHERE CURRENT_TIMESTAMP > expires;""";
+
+        await cmd.ExecuteNonQueryAsync();
+    }
+
+    private static SqliteConnection CreateConnection() => new SqliteConnection(ConnectionString);
+}
